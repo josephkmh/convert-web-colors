a60fd1cc827eb61841e1b4646b4c78110939397b
josephkmh <jmarshmenthowell@gmail.com>
2017-08-22 20:58:29 +0200

    made tests more concise and compliant with eslint rules

--- test/rgbToHex-test.js
+++ test/rgbToHex-test.js
@@ -4,18 +4,12 @@ const expect = require('chai').expect;
 const converter = require('./../index.js');
 
 describe('rgbToHex', () => {
-    it('should convert basic rgb() colors to hex (red, blue, green)', () => {
-      var red   = converter.rgbToHex("rgb(255,0,0)");
-      var green = converter.rgbToHex("rgb(0,255,0)");
-      var blue  = converter.rgbToHex("rgb(0,0,255)");
-
-      expect(red).to.equal("#FF0000");
-      expect(green).to.equal("#00FF00");
-      expect(blue).to.equal("#0000FF");
-    });
-
-    it('should convert a random rgb() color to hex', () => {
-      var hex = converter.rgbToHex("rgb(202,41,178)");
-      expect(hex).to.equal("#CA29B2");
-    });
+  it('should convert basic rgb() colors to hex (red, blue, green)', () => {
+    expect(converter.rgbToHex('rgb(255,0,0)')).to.equal('#FF0000');
+    expect(converter.rgbToHex('rgb(0,255,0)')).to.equal('#00FF00');
+    expect(converter.rgbToHex('rgb(0,0,255)')).to.equal('#0000FF');
+  });
+  it('should convert a random rgb() color to hex', () => {
+    expect(converter.rgbToHex('rgb(202,41,178)')).to.equal('#CA29B2');
+  });
 });
