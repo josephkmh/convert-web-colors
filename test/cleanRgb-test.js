4ec70960cf8c7adee8ed3390d89d65f1d7b37ff2
josephkmh <jmarshmenthowell@gmail.com>
2017-08-22 20:33:58 +0200

    fix to allow capital letters, (semi)colons and spaces in rgba/hsla input

--- test/cleanRgb-test.js
+++ test/cleanRgb-test.js
@@ -10,4 +10,7 @@ describe('cleanRgb', () => {
   it('should return an array from an rgb() string when specified', () => {
     expect(converter.cleanRgb('rgb( 255, 255, 255 )', { returnArray: true })).to.deep.equal([255, 255, 255]);
   });
+  it('should remove leading white space, capital letters, semicolons and colons', () => {
+    expect(converter.cleanRgb('  Rgb( 123, 231, 112 ) ')).to.equal('rgb(123, 231, 112)');
+  });
 });
--- test/cleanRgba-test.js
+++ test/cleanRgba-test.js
@@ -4,24 +4,19 @@ const expect = require('chai').expect;
 const converter = require('./../index.js');
 
 describe('cleanRgba', () => {
-    it('should remove whitespace from rgba()', () => {
-        var test = converter.cleanRgba('rgba( 255, 255, 255, 1 )');
-        var expected = 'rgba(255, 255, 255, 1)';
-        expect(test).to.equal(expected);
-    });
-    it('should remove whitespace from rgba() with a decimal alpha value', () => {
-        var test = converter.cleanRgba('rgba( 255, 255, 255, .5 )');
-        var expected = 'rgba(255, 255, 255, 0.5)';
-        expect(test).to.equal(expected);
-    });
-    it('should return an array from an rgba() string when specified', () => {
-        var test = converter.cleanRgba('rgba( 255, 255, 255, 1 )', {returnArray: true});
-        var expected = [255, 255, 255, 1];
-        expect(test).to.deep.equal(expected);
-    });
-    it('should return alpha channel as a decimal when given a percentage', () => {
-        var test = converter.cleanRgba('rgba( 123, 123, 123, 81.6% )', {returnArray: true});
-        var expected = [123,123,123,0.816];
-        expect(test).to.deep.equal(expected);
-    });
+  it('should remove whitespace from rgba()', () => {
+    expect(converter.cleanRgba('rgba( 255, 255, 255, 1 )')).to.equal('rgba(255, 255, 255, 1)');
+  });
+  it('should remove whitespace from rgba() with a decimal alpha value', () => {
+    expect(converter.cleanRgba('rgba( 255, 255, 255, .5 )')).to.equal('rgba(255, 255, 255, 0.5)');
+  });
+  it('should return an array from an rgba() string when specified', () => {
+    expect(converter.cleanRgba('rgba( 255, 255, 255, 1 )', { returnArray: true })).to.deep.equal([255, 255, 255, 1]);
+  });
+  it('should return alpha channel as a decimal when given a percentage', () => {
+    expect(converter.cleanRgba('rgba( 123, 123, 123, 81.6% )', { returnArray: true })).to.deep.equal([123, 123, 123, 0.816]);
+  });
+  it('should remove leading white space, capital letters, semicolons and colons', () => {
+    expect(converter.cleanRgba('  RgBA( 123, 231, 112,.34 ) ')).to.equal('rgba(123, 231, 112, 0.34)');
+  });
 });
